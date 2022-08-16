e17881f4b601424b4600fe779b1a409fe19f45dc
Tyler M <tmorse2222@gmail.com>
2022-08-16 16:18:34 -0700

    Added description prompt and info to generated README

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require(`fs`);
 const inquirer = require(`inquirer`);
 // TODO: Create an array of questions for user input
-const questions = [`What is the title of your application?`, ``];
+const questions = [`What is the title of your application?`, `Please provide a description of your application`];
 
 // TODO: Create a function to write README file
 
@@ -11,11 +11,19 @@ const questions = [`What is the title of your application?`, ``];
     name: 'title',
     message: `${questions[0]}`,
     type: 'input'
-       }])
+       },
+    {
+        name: `description`,
+        message: `${questions[1]}`,
+        type: `input`
+    }])
       .then(function(answer){
     answer1 = answer.title;
+    answer2 = answer.description;
       }).then(function(){
-      fs.writeFile(`newREADME.md`, `<h1>${answer1}</h1><br>`, function(err){
+      fs.writeFile(`newREADME.md`, `<h1>${answer1}</h1><br>
+      <h2>Description:</h2><br>
+      ${answer2}<br>`, function(err){
         if (err) throw err;
     });});
 // TODO: Create a function to initialize app
--- newREADME.md
+++ newREADME.md
@@ -1,1 +1,3 @@
-<h1>NEW CONTENT</h1>
+<h1>README GENERATOR</h1><br>
+      <h2>Description:</h2><br>
+      This application is desgined to save a developers time by automatically creating a README file for any application<br>
