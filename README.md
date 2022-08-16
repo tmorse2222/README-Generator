d2f8f67219eb83475280f0bb67077b2fde131f8c
Tyler M <tmorse2222@gmail.com>
2022-08-16 11:08:35 -0700

    Created function to create README file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# README-Generator
+Example content
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
-
+const fs = require(`fs`);
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeFile(`README.md`, `Example content`, function(err){
+    if (err) throw err;
+    console.log(`README created`);
+});
 
 // TODO: Create a function to initialize app
 function init() {}
