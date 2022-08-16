996c63abc32e67e60d2a9d0e6c465e36e843a910
Tyler M <tmorse2222@gmail.com>
2022-08-16 14:51:39 -0700

    Created first prompt in terminal

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // TODO: Include packages needed for this application
 const fs = require(`fs`);
+const inquirer = require(`inquirer`);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [`What is the title of your application?`, ``];
 
 // TODO: Create a function to write README file
 fs.writeFile(`newREADME.md`, `Example content`, function(err){
     if (err) throw err;
-    console.log(`README created`);
 });
-
+ inquirer.prompt([
+     {
+    name: 'title',
+    message: `${questions[0]}`,
+    type: 'input'
+       }])
+      .then(function(answer){
+       console.log(answer);
+      });
 // TODO: Create a function to initialize app
 function init() {}
 
