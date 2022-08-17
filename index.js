f5fb9f0eef5c244c22cac0fcc3c66894872f6b5d
Tyler M <tmorse2222@gmail.com>
2022-08-16 17:39:11 -0700

    Added usage section

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require(`fs`);
 const inquirer = require(`inquirer`);
 // TODO: Create an array of questions for user input
 const questions = [`What is the title of your application?`, `Please provide a description of your application`, `Please provide any installation requirements`,
-];
+`How should this app be operated?`];
 
 // TODO: Create a function to write README file
 
@@ -22,11 +22,17 @@ const questions = [`What is the title of your application?`, `Please provide a d
         name: `installation`,
         message: `${questions[2]}`,
         type: `input`
+    },
+    {
+        name: `usage`,
+        message: `${questions[3]}`,
+        type: `input`
     }])
       .then(function(answer){
     answer1 = answer.title;
     answer2 = answer.description;
     answer3 = answer.installation;
+    answer4 = answer.usage;
       }).then(function(){
       fs.writeFile(`newREADME.md`, `<h1>${answer1}</h1><br>
       <h2>Table of Contents:</h2><br>
@@ -39,8 +45,10 @@ const questions = [`What is the title of your application?`, `Please provide a d
       <a href="#questions">Questions</a><br>
       <h2>Description:</h2><br>
       ${answer2}<br>
-      <h2>Installation</h2><br>
-      ${answer3}<br>`, function(err){
+      <h2>Installation:</h2><br>
+      ${answer3}<br>
+      <h2>Usage:</h2><br>
+      ${answer4}<br>`, function(err){
         if (err) throw err;
     });});
 // TODO: Create a function to initialize app
